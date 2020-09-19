28aaba884a5889150d00490534cdcb99fef87230
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-09-19 01:00:54 -0500

    Parsing the file.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<fstream>
+#include<sstream>
 #include <stack>
 #include "ArgumentManager.h"
 
@@ -45,14 +46,13 @@ int precedence(char operators)
     }
 }
 
-
 //Creat a function to check if the expression is balanced
 bool checkBalanced(string expression)
 {
     stack<char> st;
 
     //Traversing the Expression
-    for (int i = 0; i < expression.length(); i++)
+    for (unsigned int i = 0; i < expression.length(); i++)
     {
         //Push the opening bracket in the stack
         if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
@@ -120,7 +120,7 @@ string infixToPostfix(string infix)
     string postfix = "";
 
     //Traversing the Expression
-    for (int i = 0; i < infix.length(); i++)
+    for (unsigned int i = 0; i < infix.length(); i++)
     {
         //If infix[i] is a digit, add it to the string
         if (isdigit(infix[i]))
@@ -211,12 +211,14 @@ int evaluatePostfix(string postfix)
     int result;
 
     //Traversing the Expression
-    for (int i = 0; i < postfix.length(); i++)
+    for (unsigned int i = 0; i < postfix.length(); i++)
     {
+        //If postfix[i] is a digit, push it to the stack
         if (isdigit(postfix[i]))
         {
             st.push(int(postfix[i]) - 48);
         }
+        //If the postfix[i] is a operand, continue to do the operation
         else
         {
             //Read the second operand and pop
@@ -225,14 +227,58 @@ int evaluatePostfix(string postfix)
             //Read the first operand and pop
             left = st.top();
             st.pop();
+
+            //Call function operation to get the result
             result = operation(left, right, postfix[i]);
+            //Push it to the stack
             st.push(result);
         }
     }
+
     return st.top();
 }
 
+//Create a function to check for colon
+bool hasColon(string str)
+{
+    for (unsigned int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ':')
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
+//Create a function to read password
+int readPassword(string str, istringstream& inSS)
+{
+    string bin;
+    string password_str;
+    int password;
+
+    //Clear input string stream
+    inSS.clear();
+
+    //Using input string stream to read password line
+    inSS.str(str);
+
+    //Get rid of the useless infomation
+    getline(inSS, bin, ':');
+
+    //Read password
+    getline(inSS, password_str);
+    password = stoi(password_str);
+
+    return password;
+}
+
+//Create a function to read expression to Scarlet Linked list
+
+
+
+// ------------------------------------------------ MAIN FUNCTION ----------------------------------------------------
 int main(int argc, char* argv[])
 {
     ArgumentManager am(argc, argv);
@@ -242,34 +288,133 @@ int main(int argc, char* argv[])
     //string output = am.get("output");
 
     //Test
-    string input = "input13.txt";
-    string output = "output13.txt";
+    string input = "input11.txt";
+    string output = "output11.txt";
 
     ifstream inFS;
     ofstream outFS;
+    istringstream inSS;
 
     inFS.open(input);
     outFS.open(output);
 
-    string check = "8-{(7+0-1)}-3";
-    string postfilx;
-
-    if (!checkBalanced(check))
+    try 
     {
-        cout << "Not balanced" << endl;
+        //Throw exception if the file doesn't exist
+        if (!inFS.is_open())
+        {
+            throw runtime_error("ERROR: File not found");
+        }
+
+        //Throw exception if the file is empty
+        if (inFS.peek() == EOF)
+        {
+            throw runtime_error("ERROR: File is empty");
+        }
+
+        string line;
+        string expression;
+        int password;
+        digitList Scarlet;
+        digitList Travis;
+
+        while (getline(inFS, line))
+        {
+            //Check if the line is empty
+            if (line.empty())
+            {
+                continue;
+            }
+
+            //Only password line contains colon
+            if (hasColon(line))
+            {
+                password = readPassword(line, inSS);
+            }
+
+            else if (line == "Scarlet")
+            {
+                while (getline(inFS, expression))
+                {
+                    //Check if the line is empty
+                    if (line.empty())
+                    {
+                        continue;
+                    }
+
+                    if (expression == "Travis" || expression == "Scarlet")
+                    {
+                        break;
+                    }
+
+                    //If the line contains colon, read the password directly
+                    if (hasColon(expression))
+                    {
+                        password = readPassword(expression, inSS);
+                        break;
+                    }
+
+                    //Read in the linked list
+                    cout << "Scarlet" << endl;
+
+                }
+            }
+
+            else if (line == "Travis")
+            {
+                while (getline(inFS, expression))
+                {
+                    if (expression == "Scarlet" || expression == "Travis")
+                    {
+                        break;
+                    }
+
+                    //If the line contains colon, read the password directly
+                    if (hasColon(expression))
+                    {
+                        password = readPassword(expression, inSS);
+                        break;
+                    }
+
+                    //Read in the linked list
+                    cout << "Travis" << endl;
+                }
+            }
+        }
+
+
+
+
+
+
     }
-    else
+    catch (runtime_error & e)
     {
-        postfilx = infixToPostfix(check);
+        outFS << e.what() << endl;
     }
 
+    //string check = "8-{(7+0-1)}-3";
+    //string postfix;
+    //int result;
 
-    cout << postfilx << endl;
+    //if (!checkBalanced(check))
+    //{
+    //    cout << "Not balanced" << endl;
+    //}
+    //else
+    //{
+    //    postfix = infixToPostfix(check);
+    //}
+
+    //result = evaluatePostfix(postfix);
+    //cout << postfix << ": " << result << endl;
 
 
+    //string test = "1234:";
+    //cout << hasColon(test) << endl;
 
 
-
+    //Close all files
     inFS.close();
     outFS.close();
 
